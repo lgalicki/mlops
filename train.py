353ec9bb3ef894656bd875b5cb194a3739ec199a
Luciano Galicki <lgalicki@gmail.com>
2023-08-29 21:00:57 -0300

    add test_train

--- train.py
+++ train.py
@@ -20,29 +20,37 @@ import numpy as np
 
 
 def reset_seeds():
-   os.environ['PYTHONHASHSEED']=str(42)
-   tf.random.set_seed(42)
-   np.random.seed(42)
-   random.seed(42)
+    os.environ['PYTHONHASHSEED'] = str(42)
+    tf.random.set_seed(42)
+    np.random.seed(42)
+    random.seed(42)
 
 
 def process_data():
-    data = pd.read_csv('https://raw.githubusercontent.com/renansantosmendes/lectures-cdas-2023/master/fetal_health_reduced.csv')
-
-    X=data.drop(["fetal_health"], axis=1)
-    y=data["fetal_health"]
+    X, y = read_data()
 
     columns_names = list(X.columns)
     scaler = preprocessing.StandardScaler()
     X_df = scaler.fit_transform(X)
     X_df = pd.DataFrame(X_df, columns=columns_names)
 
-    X_train, X_test, y_train, y_test = train_test_split(X_df, y, test_size=0.3, random_state=42)
+    X_train, X_test, y_train, y_test =\
+        train_test_split(X_df, y, test_size=0.3, random_state=42)
 
-    y_train = y_train -1
+    y_train = y_train - 1
     y_test = y_test - 1
 
     return X_train, X_test, y_train, y_test
+
+
+def read_data():
+    data = pd.read_csv(
+        'https://raw.githubusercontent.com/'
+        'renansantosmendes/lectures-cdas-2023'
+        '/master/fetal_health_reduced.csv')
+    X = data.drop(["fetal_health"], axis=1)
+    y = data["fetal_health"]
+    return X, y
 
 
 def create_model(train_data):
@@ -75,7 +83,7 @@ def config_mlflow():
 
 def train_model(model, X_train, y_train):
     with mlflow.start_run(run_name='experiment_01') as run:
-      model.fit(X_train, y_train, epochs=50, validation_split=0.2, verbose=3)
+        model.fit(X_train, y_train, epochs=50, validation_split=0.2, verbose=3)
 
 
 if __name__ == '__main__':
